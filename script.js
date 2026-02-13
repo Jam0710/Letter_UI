ef252c86f7b1370f17b3f2c57b3f4f7714955bbb
Jam0710 <jerome.majarian@web.de>
2026-02-13 14:02:52 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const letter = document.getElementById("letter");
-
-document.getElementById("openBtn").addEventListener("click", () => {
-  letter.classList.add("open");
-});
-
-document.getElementById("closeBtn").addEventListener("click", () => {
-  letter.classList.remove("open");
-});
